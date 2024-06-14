7297d5965ce858f0c2d01fd12ea2404d7c6500d1
AntonioDattolo <antonio19794.a@gmail.com>
2024-06-14 16:25:40 +0200

    M1 + M2

--- js/main.js
+++ js/main.js
@@ -5,29 +5,38 @@ const myConfig = {
 	data() {
 		
 		return {
-			todolist : [ "ciao"],
-			addTask : null
+			todolist: ["ciao"],
+			addTask: null,
+			requestConfig: {
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
+			}
 			
 		}
 	},
 	methods: {
 		getDoit(index){
 			console.log(this.todolist)
-			if(this.todolist[index].state != 'doit'){
-				this.todolist[index].state = 'doit'
+			if(this.todolist[index].state != true){
+				this.todolist[index].state = true
 			} else{
-				this.todolist[index].state = 'loading'
+				this.todolist[index].state = false
 			}
 		},
 		getAddTask(){
-			let task = new Object({name : this.addTask, state : "loading"})
-			this.todolist.push(task)
+			let task = new Object({name : this.addTask, state : false})
+			axios.post("../php-todo-list-json/server.php",task,this.requestConfig).then(results => {
+				console.log("new Task :", results.data)
+				this.todolist = results.data
+			})
+			this.addTask= null
 		}
 	},
 	mounted(){
 		window.vue = this;
 		axios.get("../php-todo-list-json/server.php").then(results => {
-			console.log("Risultati: ", results);
+			console.log("Risultati: ", results.data);
 			this.todolist = results.data;
 		})
 	}
--- server.php
+++ server.php
@@ -1,29 +1,46 @@
 <?php
-$todolist = [
-    [
-        'name' => 'fare la spesa',
-        'state' => 'loading'
-    ],
-    [
-        'name' => 'portare fuori il cane',
-        'state' => 'loading'
-    ],
-    [
-        'name' => 'mettere la lavatrice',
-        'state' => 'loading'
-    ],
-    [
-        'name' => 'lavare i piatti',
-        'state' => 'loading'
-    ],
-    [
-        'name' => 'controllare email',
-        'state' => 'doit'
-    ],
-];
+// $todolist = [
+//     [
+//         'name' => 'fare la spesa',
+//         'state' => 'loading'
+//     ],
+//     [
+//         'name' => 'portare fuori il cane',
+//         'state' => 'loading'
+//     ],
+//     [
+//         'name' => 'mettere la lavatrice',
+//         'state' => 'loading'
+//     ],
+//     [
+//         'name' => 'lavare i piatti',
+//         'state' => 'loading'
+//     ],
+//     [
+//         'name' => 'controllare email',
+//         'state' => 'doit'
+//     ],
+// ];
 
-header('Access-Control-Allow-Headers: Origin, Content-Type: application/json');
+if (isset($_POST["name"]) && isset($_POST["state"])) {
+    $jsonString = file_get_contents("dati.json");
+    $todolist = json_decode($jsonString, true);
+    $newtodo = [
+        "name" => $_POST["name"],
+        "state" => $_POST["state"]
+    ];
+    $todolist[] = $newtodo;
+    $todolistJson = json_encode($todolist);
+    file_put_contents("dati.json", $todolistJson);
+    echo $todolistJson;
+} else {
 
-$jsonString = json_encode($todolist);
 
-echo $jsonString;
+    header('Content-Type: application/json');
+
+    // $jsonString = json_encode($todolist);
+    $jsonString = file_get_contents("dati.json");
+    // header('Access-Control-Allow-Headers: Origin, Content-Type: application/json');
+
+    echo $jsonString;
+}
